9c98a8ca6f9b241383db08453da861dd0e8e10eb
carlos1377 <113457040+carlos1377@users.noreply.github.com>
2023-04-26 16:52:41 -0300

    Add files via upload

---
+++ facul/exer_rep4/Program.cs
@@ -0,0 +1,75 @@
+﻿int num1, num2, i;
+Console.WriteLine("Digite um número inteiro e positivo: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite outro número inteiro e positivo: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+if(num2 < 0 | num1 < 0){
+    Console.WriteLine("Esse número não é inteiro.");
+    return;
+}
+for(i = num1; i <= num2; i++){
+    int par = i % 2;
+    if(par == 0){
+        Console.WriteLine($"{i} de {num2}");
+    } 
+}
+int num3, num4;
+Console.WriteLine("Digite um número inteiro e positivo: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite outro número inteiro e positivo: ");
+num4 = Convert.ToInt32(Console.ReadLine());
+if(num3 < 0 | num4 < 0){
+    Console.WriteLine("Esse número não é inteiro.");
+    return;
+}
+for(i = num3; i <= num4; i++){
+    int par = i % 2;
+    if(par == 0){
+        Console.WriteLine($"{i} de {num4}");
+    } 
+}
+int num5, num6;
+Console.WriteLine("Digite um número inteiro e positivo: ");
+num5 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite outro número inteiro e positivo: ");
+num6 = Convert.ToInt32(Console.ReadLine());
+if(num5 < 0 | num6 < 0){
+    Console.WriteLine("Esse número não é inteiro.");
+    return;
+}
+for(i = num5; i <= num6; i++){
+    int par = i % 2;
+    if(par == 0){
+        Console.WriteLine($"{i} de {num6}");
+    } 
+}
+int num7, num8;
+Console.WriteLine("Digite um número inteiro e positivo: ");
+num7 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite outro número inteiro e positivo: ");
+num8 = Convert.ToInt32(Console.ReadLine());
+if(num7 < 0 | num8 < 0){
+    Console.WriteLine("Esse número não é inteiro.");
+    return;
+}
+for(i = num7; i <= num8; i++){
+    int par = i % 2;
+    if(par == 0){
+        Console.WriteLine($"{i} de {num8}");
+    } 
+}
+int num9, num10;
+Console.WriteLine("Digite um número inteiro e positivo: ");
+num9 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite outro número inteiro e positivo: ");
+num10 = Convert.ToInt32(Console.ReadLine());
+if(num9 < 0 | num10 < 0){
+    Console.WriteLine("Esse número não é inteiro.");
+    return;
+}
+for(i = num9; i <= num10; i++){
+    int par = i % 2;
+    if(par == 0){
+        Console.WriteLine($"{i} de {num10}");
+    } 
+}
---
+++ facul/exer_rep5/Program.cs
@@ -0,0 +1,34 @@
+﻿int alunoMaisAlto = 0;
+int alturaAlunoMaisAlto = 0;
+int alunoMaisBaixo = 0;
+int alturaAlunoMaisBaixo = 0;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.Write($"Informe a altura do aluno {i}: ");
+    int altura = Convert.ToInt32(Console.ReadLine());
+
+    if (i == 1)
+    {
+        alunoMaisAlto = i;
+        alturaAlunoMaisAlto = altura;
+        alunoMaisBaixo = i;
+        alturaAlunoMaisBaixo = altura;
+    }
+    else
+    {
+        if (altura > alturaAlunoMaisAlto)
+        {
+            alunoMaisAlto = i;
+            alturaAlunoMaisAlto = altura;
+        }
+        if (altura < alturaAlunoMaisBaixo)
+        {
+            alunoMaisBaixo = i;
+            alturaAlunoMaisBaixo = altura;
+        }
+    }
+}
+
+Console.WriteLine($"O aluno mais alto é o {alunoMaisAlto}, com {alturaAlunoMaisAlto} cm de altura.");
+Console.WriteLine($"O aluno mais baixo é o {alunoMaisBaixo}, com {alturaAlunoMaisBaixo} cm de altura.");
